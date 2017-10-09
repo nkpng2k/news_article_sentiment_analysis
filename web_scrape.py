328abf65b9f07cbafbfb8b4822f814aaca4a533c
Nick Png <nkpng2k@gmail.com>
2017-10-08 22:31:20 -0700

    should be done

--- web_scrape.py
+++ web_scrape.py
@@ -4,50 +4,83 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from collections import defaultdict
 import time
+import pymongo
 
-def retrieve_key(file_path, api):
-    with open(file_path) as f:
-        for line in f:
-            if line.startswith(api):
-                api_key = line.split(':')[1].strip()
-        return api_key
+# def retrieve_key(file_path, api):
+#     with open(file_path) as f:
+#         for line in f:
+#             if line.startswith(api):
+#                 api_key = line.split(':')[1].strip()
+#         return api_key
+#
+#
+# def single_query(link, payload):
+#     response = requests.get(link, params=payload)
+#     if response.status_code != 200:
+#         print 'WARNING', response.status_code
+#     else:
+#         return response.json()
 
-
-def single_query(link, payload):
-    response = requests.get(link, params=payload)
-    if response.status_code != 200:
-        print 'WARNING', response.status_code
-    else:
-        return response.json()
 
 def run_selenium(base_url, topics):
-    driver = webdriver('~/.ssh/chromedriver')
+    driver = webdriver.Chrome('/Users/npng/.ssh/chromedriver')
     url_dict = defaultdict(list)
     for topic in topics:
         driver.get(base_url + topic)
+        button = True
         driver.find_element_by_css_selector('button.button.load-more-button').click()
+        for i in xrange(10):
+            print 'scrolling once'
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(5)
         urls = driver.find_elements_by_class_name('story-link')
         for url in urls:
             url_dict[topic].append(str(url.get_attribute('href')))
+    driver.quit()
     return url_dict
 
 
-def scrape(url_dictionary):
+def one_request(url, topic):
+    article_dict = defaultdict(list)
+    req = requests.get(url)
+    if response.status_code != 200:
+        print 'WARNING', response.status_code
+    else:
+        soup = BeautifulSoup(req.text, 'html.parser')
+        headline = soup.find_all('h1', class_='headline')[0].contents[0]
+        author = soup.find_all('span', class_= 'byline-author')[0]['data-byline-name']
+        date = soup.find_all('time', class_= 'dateline')[0]['datetime']
+        paragraphs = soup.find_all('p', class_='story-body-text story-content')
+        article = ''
+        for p in paragraphs:
+            article = article + p.get_text()
+
+        for item in [author, date, article, topic]:
+            article_dict[headline].append(item)
+
+    return article_dict
+
+
+def scrape(url_dictionary, coll):
     topics = url_dictionary.keys()
     for top in topics:
+        print 'Starting --> Topic: {}'.format(top)
         url_list = url_dictionary[top]
         for url in url_list:
-            req = requests.get(url)
-            soup = BeautifulSoup(req.text)
-            #NOTE: need to get headline and author
-            paragraphs = soup.find_all('p', class_='story-body-text story-content')
-            article = ''
-            for p in paragraphs:
-                article = article + p.get_text()
-            #NOTE: have single article, author, and headline --> add this to a list?
-            
-            time.sleep(15)
+            print 'Starting --> Topic: {} --- URL: {}'.format(top, url)
+            art = one_request(url, top)
+            coll.insert_one(art)
+            print 'Moving To Next Article in 30 seconds'
+            time.sleep(30)
+        print 'COMPLETE ALL ARTICLES'
+    print 'COMPLETE ALL TOPICS'
 
+def launch_mongo(database, collection, uri = None):
+    mc = pymongo.MongoClient(uri)
+    db = mc[database]
+    coll = db[collection]
+
+    return coll
 
 
 if __name__ == '__main__':
@@ -58,9 +91,12 @@ if __name__ == '__main__':
     # payload = {'api-key': api_key}
     # html_str = single_query(link, payload)
 
+    mongo_coll = launch_mongo('news_articles', 'nyt_articles')
     topics = ['politics', 'business', 'world', 'us', 'science', 'health']
     base_url = 'https://www.nytimes.com/section/'
     url_dict = run_selenium(base_url, topics)
+    scrape(url_dict, mongo_coll)
+
 
 
 
